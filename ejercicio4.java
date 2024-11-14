5ae9acf3184f26b82405e18f72f8eed02f3a50e8
MARLON XAVIER DELGADO RUIZ <marlonxavi6@gmil.com>
2024-11-14 15:49:45 -0500

    Se realiza el ejercicio 4

---
+++ ejercicio4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+        var entrada = new Scanner (System.in);
+        char letra;
+
+        System.out.println("Ingrese una letra: ");
+        letra = entrada.next().charAt(0);
+
+        if (Character.isUpperCase(letra)) {
+            System.out.println("La letra que ingresaste está en mayúscula");
+        }else{
+            System.out.println("La letra que ingresaste está en minúscula");
+        }
+    }
+}
