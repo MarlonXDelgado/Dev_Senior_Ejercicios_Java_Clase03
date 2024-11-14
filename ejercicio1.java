7c34bd9881dc4be1413bb5d6bfe96a47c81c4991
MARLON XAVIER DELGADO RUIZ <marlonxavi6@gmil.com>
2024-11-14 09:38:15 -0500

    Se realiza el ejercicio 1

---
+++ ejercicio1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+        
+        var scanner = new Scanner(System.in);
+        System.out.println("Ingrese un número: ");
+        var numero = scanner.nextFloat();
+
+        if(numero < 0){
+            System.out.printf("El número: %.0f, es un número negativo.", numero);
+        }
+        if(numero == 0){
+            System.out.printf("El número es %.0f.", numero);
+        } if(numero > 0){
+            System.out.printf("El número: %.0f, es un número positivo.", numero);
+        }
+        
+    }
+
+}
