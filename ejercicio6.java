92ee53a6a76a1c8d8566339bbda644db48dc1501
Marlon Xavier Delgado Ruiz <marlonxavi6@gmail.com>
2024-11-18 18:51:33 -0500

    Se crea el ejercicio 6

--- ejercicio6.java
+++ ejercicio6.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ejercicio6 {
     public static void main(String[] args) {
-        
+        var entrada = new Scanner(System.in);
+        int numero;
+
+        System.out.println("Ingrese un numero: ");
+        numero = entrada.nextInt();
+
+        if (numero % 2 == 0 && numero % 7 == 0) {
+            System.out.printf("%nEl numer:o %d que ingresaste, si es divisible por 7%n", numero);
+        } else{
+            System.out.printf("%nEl numero: %d que ingresaste, no es divisible por 7%n", numero);
+        }
+
+        entrada.close();
     }
 }
