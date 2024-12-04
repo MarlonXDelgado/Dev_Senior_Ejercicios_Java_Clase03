2bdef9231a5907212d9aea726c127528853cb1dc
MARLON XAVIER DELGADO RUIZ <marlonxavi6@gmil.com>
2024-12-04 14:01:24 -0500

    Se crea el ejercicio 19

---
+++ ejercicio19.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ejercicio19 {
+    public static void main(String[] args) {
+        var entrada = new Scanner(System.in);
+        int numerosSucesion;
+        ArrayList<Integer> fibonacci = new ArrayList<>(Arrays.asList(0,1));
+
+        System.out.println("Ingrese la cantidad de numeros de la secuenia de fibonacci que desea ver: ");
+        numerosSucesion = entrada.nextInt();
+
+        for (int i = 0; i < numerosSucesion; i++) {
+            
+        }
+    }
+}
